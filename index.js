0efa90ad4b4ea11d1a1df30e375d78ec973cfbf1
Diogo Costa <dsgmcosta@gmail.com>
2019-02-03 10:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i = 0; i < musicians.length; i++) {
-    array[i] = `${musicians[i]} plays ${instruments[i]}.`
+    array[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   return array
 }
