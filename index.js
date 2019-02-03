7811e50c15d471c4d8b4932f227ed7b5abb98d55
Diogo Costa <dsgmcosta@gmail.com>
2019-02-03 10:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i++) {
+    array[i] = `${musicians[i]} plays ${instruments[i]}.`
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  
+}
